feeaffdf80a158b0b832f20ff5d38f9d0665cf93
김반달 <vndlsmacmini@gimbandal-ui-Macmini.local>
2025-05-18 16:07:10 +0900

    mapmap

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
-import styled from "styled-components";
 import { Divider } from "antd";
+import styled from "styled-components";
 import Flower from "../assets/flower2.png";
 
 const Wrapper = styled.div`
@@ -35,38 +35,63 @@ const Content = styled.p`
   margin: 0;
 `;
 
-const MapContainer = styled.div`
+const Map = styled.div`
   width: 100%;
-  height: 360px;
-  min-height: 360px;
+  padding: 0;
 `;
 
 const Location = () => {
-  useEffect(() => {
-    const initMap = () => {
-      window.kakao.maps.load(() => {
-        const container = document.getElementById("map");
-        const options = {
-          center: new window.kakao.maps.LatLng(36.815107, 127.113833),
-          level: 3,
-        };
-        new window.kakao.maps.Map(container, options);
-      });
-    };
+  // 카카오 맵 불러오기
 
-    if (window.kakao && window.kakao.maps && window.kakao.maps.load) {
-      initMap();
-    } else {
-      const script = document.createElement("script");
-      script.src =
-        "//dapi.kakao.com/v2/maps/sdk.js?appkey=fab35880c5a5e1f6b78fc0cd1cdedf4c&autoload=false";
-      script.async = true;
-      script.onload = () => {
-        initMap();
+  // <!-- 3. 실행 스크립트 -->
+  const executeScript = () => {
+    const scriptTag = document.createElement("script");
+    const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
+    "timestamp" : "1747551838882",
+    "key" : "2o32y",
+    "mapWidth" : "640",
+    "mapHeight" : "360"
+  }).render();`);
+    scriptTag.appendChild(inlineScript);
+    document.body.appendChild(scriptTag);
+  };
+
+  // <!-- 2. 설치 스크립트 * 지도 퍼가기 서비스를 2개 이상 넣을 경우, 설치 스크립트는 하나만 삽입합니다. -->
+  // document.write 문제가 발생해서 해당 파일을 직접 가져온다음 수정했음
+  const InstallScript = () => {
+    (function () {
+      let c = window.location.protocol === "https:" ? "https:" : "http:";
+      let a = "16137cec";
+
+      if (window.daum && window.daum.roughmap && window.daum.roughmap.cdn) {
+        return;
+      }
+      window.daum = window.daum || {};
+      window.daum.roughmap = {
+        cdn: a,
+        URL_KEY_DATA_LOAD_PRE: c + "//t1.daumcdn.net/roughmap/",
+        url_protocal: c,
       };
-      document.head.appendChild(script);
-    }
-  }, []);
+      let b =
+        c +
+        "//t1.daumcdn.net/kakaomapweb/place/jscss/roughmap/" +
+        a +
+        "/roughmapLander.js";
+
+      // document.write -> doumnet.body.append로 수정
+      const scriptTag = document.createElement("script");
+      scriptTag.src = b;
+      document.body.append(scriptTag);
+      scriptTag.onload = () => {
+        executeScript();
+      };
+    })();
+  };
+
+  useEffect(() => {
+    InstallScript();
+  }, [InstallScript]);
+
 
   return (
     <Wrapper>
